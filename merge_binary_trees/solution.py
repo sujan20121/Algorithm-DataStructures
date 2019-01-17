d255fc6f27fa2267775db7cb6d28ec3a62e4bf4a
sujan20121 <emailsujanbhattarai@gmail.com>
2019-01-16 23:15:36 -0600

    solution for adding two binary trees

--- merge_binary_trees/solution.py
+++ merge_binary_trees/solution.py
@@ -1,20 +1,71 @@
-class TreeNode(object):
-	def __init__(self,x):
-		self.val = x
+class TreeNode:
+	def __init__(self,data):
+		self.data = data
 		self.left = None
 		self.right = None
 
-class solution(object):
-	def mergeTrees(self,t1,t2):
 
-		if t1 and t2:
-			root = TreeNode(t1.val + t2.val)
-			root.left = self.mergeTrees(t1.left + t2.left)
-			root.right = self.mergeTrees(t1.right + t2.right)
-			return root
-		else:
-			return t1 or t2
+
+	def printTree(self):
+		if self.left:
+			
+			self.left.printTree()
+		print(self.data),
+
+		if self.right:
+			
+			self.right.printTree()
+
+
+def mergeTrees(t1,t2):
+
+	if t1 is None:
+		return t2
+	if t2 is None:
+		return t1
+	
+	t1.data += t2.data
+	
+	t1.left = mergeTrees(t1.left,t2.left)
+	
+	t1.right = mergeTrees(t1.right,t2.right)
+	return t1
+
+	
+
+
 		
 if __name__ == '__main__':
 	print("In main")
-	t1 = TreeNode()
+	tree1 = TreeNode(1)
+	tree1.left = TreeNode(3)
+	tree1.right = TreeNode(2)
+	tree1.left.left = TreeNode(5)
+	print(tree1.data)
+	print(tree1.left.data)
+	print(tree1.right.data)
+	print(tree1.left.left.data)
+	print('\n')
+	
+	tree2 = TreeNode(2)
+	tree2.left = TreeNode(1)
+	tree2.right = TreeNode(3)
+	tree2.left.right = TreeNode(4)
+	tree2.right.right = TreeNode(7)
+	
+	
+	print(tree2.data)
+	print(tree2.left.data)
+	print(tree2.right.data)
+	print(tree2.left.right.data)
+	print(tree2.right.right.data)
+
+	merged = mergeTrees(tree2,tree1)
+	print('\n')
+	print(merged.data)
+	print(merged.left.data)
+	print(merged.right.data)
+	print(merged.left.left.data)
+	print(merged.left.right.data)
+	print(merged.right.right.data)
+
