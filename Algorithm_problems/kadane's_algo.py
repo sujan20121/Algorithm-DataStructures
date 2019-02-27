203ac203481841a15d43163a9119a2547e2a3b8a
SUJAN BHATTARAI <emailsujanbhattarai@gmail.com>
2019-02-27 13:03:27 -0600

    Update kadane's_algo.py
    The previous commit didn't consider cases where array would consist of all negative integers

--- Algorithm_problems/kadane's_algo.py
+++ Algorithm_problems/kadane's_algo.py
@@ -1,8 +1,7 @@
 def maxSubArraySum(A):
-...     max_ending_here = 0
-...     max_so_far = 0
-...     for i in range(len(A)):
-...             max_ending_here += A[i]
-...             max_ending_here = max(max_ending_here,0)
-...             max_so_far = max(max_ending_here,max_so_far)
-...     return max_so_far
+  current_max = A[0]
+  max_so_far = A[0]
+  for i in range(1,len(A)):
+    current_max = max(A[i],current_max+A[i])
+    max_so_far = max(max_so_far,current_max)
+  return max_so_far
