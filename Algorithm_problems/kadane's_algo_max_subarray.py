892b44c3e7be8add502f130bf5f76885c064bb95
SUJAN BHATTARAI <emailsujanbhattarai@gmail.com>
2019-02-27 15:15:59 -0600

    Update kadane's_algo_max_subarray.py
    Changing the position of the comparision of A[i] and current_max

--- Algorithm_problems/kadane's_algo_max_subarray.py
+++ Algorithm_problems/kadane's_algo_max_subarray.py
@@ -3,9 +3,11 @@ def maxSubArray(A):
   current_max = A[0]
   max_so_far = A[0]
   for i in range(1,len(A)):
-    if(A[i]>A[i]+current_max and A[i]>current_max):
+    if(A[i]>A[i]+current_max):
+      if(A[i]>current_max):
+        start = i
       current_max = A[i]
-      start = i
+      
     else:
       current_max = A[i] + current_max
       
