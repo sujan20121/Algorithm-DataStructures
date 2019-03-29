4b7f1bde1465e895141e4c2ffa9f0655068f8c86
SUJAN BHATTARAI <emailsujanbhattarai@gmail.com>
2019-03-29 16:10:09 -0500

    Create string_permutation.py

---
+++ Algorithm_problems/string_permutation.py
@@ -0,0 +1,23 @@
+"""
+A Python code to generate permuations of a given string using a recursive approach
+"""
+
+def permu_generator(input_string):
+  if len(input_string) == 1:
+    return [input_string]
+  
+  else:
+    letters_except_last = input_string[:len(input_string)-1]
+    last_letter = input_string[-1]
+    
+    permu_of_letters_except_last = permu_generator(letters_except_last)#get all the permutations of the input string except the last letter
+    
+    permutations = []
+    
+    for perms in permu_of_letters_except_last:
+      for i in range(len(perms)):
+        permutations.append(perms[:i] + last_letter + perms[i:])
+        
+    return permutations
+    
+    
